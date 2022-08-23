ca81064668b7f8c09b448fa81871ef6cd17afcf7
WAMT <gnflfh@gmail.com>
2022-08-23 18:51:01 +0900

    #3 FIN
    1. addEventListener
    2. classList.toggle
    3. JS division CSS
    4. document
    5. document.querySelector
    6. create Event Function

--- momentum/app.js
+++ momentum/app.js
@@ -21,18 +21,94 @@
 // const divideResult = calculator.do(timeseResult, plusResult);
 // const powerResult = calculator.zex(divideResult, minusResult);
 
-const age = parseInt(prompt('How old are you?'));
+//------------------------------------------------------------------
 
-if (isNaN(age) || age < 0) {
-	alert('plz write a real positive number');
-} else if (age < 18) {
-	alert('You are too young');
-} else if (age >= 18 && age <= 50) {
-	alert('You can drink');
-} else if (age > 50 && age <= 80) {
-	alert('you should exercise');
-} else if (age > 80) {
-	alert('You can do whatever you want');
-} else {
-	alert('You can drink');
-}
+// const age = parseInt(prompt('How old are you?'));
+
+// if (isNaN(age) || age < 0) {
+// 	alert('plz write a real positive number');
+// } else if (age < 18) {
+// 	alert('You are too young');
+// } else if (age >= 18 && age <= 50) {
+// 	alert('You can drink');
+// } else if (age > 50 && age <= 80) {
+// 	alert('you should exercise');
+// } else if (age == 100) {
+// 	alert('wow your wise');
+// } else if (age > 80) {
+// 	alert('You can do whatever you want');
+// } else {
+// 	alert('You can drink');
+// }
+
+// if(isNaN(age) || age < 0){
+// 	alert("plze");
+// } else if (age < 19 || age >120 ){
+// 	alert("you ca");
+// } else if (age <)
+
+//------------------------------------------------------------------
+
+// const h1 = document.querySelector('.hello h1');
+
+// const mouseClickEvent = () => {
+// 	h1.style.color = 'blue';
+// };
+// function windowCopyEvent() {
+// 	h1.style.color = 'red';
+// 	alert("don't copy that!!");
+// }
+// const mouseenterEvent = () => {
+// 	h1.innerText = 'mouse is here';
+// };
+// const mouseleaveEvent = () => {
+// 	h1.innerText = 'mouse is gone';
+// };
+// const windowResizeEvent = () => {
+// 	document.body.style.backgroundColor = 'tomato';
+// };
+// const windowoffEvent = () => {
+// 	alert('window out T T');
+// };
+// const windowonlEvent = () => {
+// 	alert('window on !!');
+// };
+// h1.addEventListener('click', mouseClickEvent);
+// h1.addEventListener('mouseenter', mouseenterEvent);
+// h1.addEventListener('mouseleave', mouseleaveEvent);
+// window.addEventListener('resize', windowResizeEvent);
+// window.addEventListener('copy', windowCopyEvent);
+// window.addEventListener('offline', windowoffEvent);
+// window.addEventListener('online', windowonlEvent);
+
+// h1.innerText = 'Grab me~';
+// alert(h1.innerText);
+// alert(h1.className);
+// h1.innerText = 'Got You!';
+// alert(h1.innerText);
+
+//------------------------------------------------------------------
+
+// const h1 = document.querySelector('.hello h1');
+
+// const clickEvent = () => {
+// 	const A = h1.style.color;
+// 	if (A == 'blue') {
+// 		B = 'tomato';
+// 	} else if (A == 'tomato') {
+// 		B = 'yellow';
+// 	} else {
+// 		B = 'blue';
+// 	}
+// 	h1.style.color = B;
+// };
+
+// h1.addEventListener('click', clickEvent);
+
+const h1 = document.querySelector('.hello h1');
+
+const clickEvent = () => {
+	h1.classList.toggle('clicked');
+};
+
+h1.addEventListener('click', clickEvent);
