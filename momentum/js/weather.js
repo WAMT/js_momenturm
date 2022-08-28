5c9d998b2ee58f63144e1262f43776943a3b8968
WAMT <gnflfh@gmail.com>
2022-08-29 05:05:21 +0900

    FINSH
    Wether
    To DO
    다시보기

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,9 +1,8 @@
-const API_KEY = 'a161b4b2515e6e68c53a2742576b8167';
-
+const WEATHER_API = config.apikey;
 function onGeoOk(position) {
 	const lat = position.coords.latitude;
 	const lon = position.coords.longitude;
-	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API}&units=metric`;
 	fetch(url)
 		.then((response) => response.json())
 		.then((data) => {
